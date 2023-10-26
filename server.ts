f062bd91b3f3632a3ee4cb3e678c2c4c980044c0
nus3 <hadayota33@gmail.com>
2023-10-26 14:58:25 +0900

    bind r2

--- server.ts
+++ server.ts
@@ -2,11 +2,12 @@ import { logDevReady } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
 
+// @ts-ignore
 if (process.env.NODE_ENV === "development") {
   logDevReady(build);
 }
 
-export const onRequest = createPagesFunctionHandler({
+export const onRequest: PagesFunction = createPagesFunctionHandler({
   build,
   getLoadContext: (context) => ({ env: context.env }),
   mode: build.mode,
